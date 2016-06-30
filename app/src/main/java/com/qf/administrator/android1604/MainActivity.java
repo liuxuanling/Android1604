3b4c1f81af7a6a363c787539590d02ecb50fec5c
liuxuanling <734604239@qq.com>
2016-06-30 16:08:20 +0800

    第一个Android项目

--- app/src/main/java/com/qf/administrator/android1604/MainActivity.java
+++ app/src/main/java/com/qf/administrator/android1604/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    Bundle bundle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
