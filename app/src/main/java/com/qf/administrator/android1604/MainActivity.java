bc9a5d57c7bfbeec10ae39375091d0a4ec27e73b
liuxuanling <734604239@qq.com>
2016-06-30 16:05:26 +0800

    第一个Android项目

---
+++ app/src/main/java/com/qf/administrator/android1604/MainActivity.java
@@ -0,0 +1,15 @@
+package com.qf.administrator.android1604;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
